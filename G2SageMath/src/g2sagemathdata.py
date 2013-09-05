c95479ae7bef488374fe8d76ec83efcc8a176811
Tharindu Rusira <tharindurusira@gmail.com>
2013-09-06 00:13:33 +0530

    g2sagemathdata.py implementation started

--- G2SageMath/src/g2sagemathdata.py
+++ G2SageMath/src/g2sagemathdata.py
@@ -4,6 +4,8 @@ Created on Sep 4, 2013
 @author: tharindurusira
 '''
 
+from sys import platform as _userplatform
+
 class DataHandler(object):
     '''
     classdocs
@@ -15,7 +17,22 @@ class DataHandler(object):
         Constructor
         '''
     def downloadCSV(self,data_arg):
-        pass
-    
-    
         
+        if(len(data_arg)==4):
+            
+            client= data_arg[0]
+            ssf=data_arg[1]
+            sskey=data_arg[2]
+            wskey=data_arg[3]
+            
+            # make an HTTP request and download the file to
+            
+            
+            if _userplatform == 'linux' or _userplatform == 'linux2':
+                #download the file to some location
+                pass
+            elif _userplatform == 'win32':
+                #download to another location
+                pass
+            
+            
