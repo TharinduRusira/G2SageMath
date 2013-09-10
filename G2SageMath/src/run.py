222dbd5074ecc47a5acb7b1ca1473aebcbae35f2
Tharindu Rusira <tharindurusira@gmail.com>
2013-09-10 23:46:19 +0530

    code from tharindu_downloadCSV branch

--- G2SageMath/src/run.py
+++ G2SageMath/src/run.py
@@ -1,17 +1,20 @@
 from g2sagemathlogin import LogIn
+from g2sagemathdata import DataHandler
 import sys
 import gflags
 
 def main(argv):
     # Let the gflags module process the command-line arguments
     l=LogIn()
+    m=DataHandler()
+    
     try:
         argv = l.FLAGS(argv)        
         x=l.login()
         y= l.showDrive(x)
         z=l.userChoice(y)
         final_details = l.readWorksheets(z)
-        print final_details
+        m.downloadCSV(data_arg=final_details)
         
     except gflags.FlagsError, e:
         print '%s\\nUsage: %s ARGS\\n%s' % (e, argv[0], l.FLAGS)
--- G2SageMath/tests/Test_g2sagemathlogin.py
+++ G2SageMath/tests/Test_g2sagemathlogin.py
@@ -4,17 +4,28 @@ Created on Aug 17, 2013
 @author: tharindurusira
 '''
 import unittest
-import glogin
+import g2sagemathlogin
+
 
 class LoginTest(unittest.TestCase):
     
-    client=''
-    ssf=''
-
+    # CLASS VARIABLES
+    # defined as global variables because we are testing units here
+    # if these are declared to be returned from another test case, -
+    # - succeeding tests will depend on the previous tests
+    # otherwise login will be needed for every other test case
+        
+    @classmethod
+    def setUpClass(self):
+        self.l1=g2sagemathlogin.LogIn()
+        self.c=self.l1.login()
+        self.ss_client = self.c[0]
+        self.doc_client=self.c[1]
+           
     
     '''
     Test id= 1
-        Unit to test = glogin.LogIn.login()
+        Unit to test = g2sagemathlogin.LogIn.login()
         Assumptions= correct authentication code is given
         Test data= a valid authentication code
         Steps 
@@ -24,22 +35,45 @@ class LoginTest(unittest.TestCase):
         Expected result= a value of type "gdata.spreadsheet.service.SpreadsheetsService"
     '''
     
-    def test_login(self):
-        print "---test_login---"
-        l1=glogin.LogIn()
+    def test_login_output0(self):
+        
         try:
-            c=l1.login()
-            self.client = c[0]
-            self.assertEqual(str(type(self.client)),"<class 'gdata.spreadsheet.service.SpreadsheetsService'>")
-            
-        except Exception as e:
-            print "Test 1: login Exception captured successfully"
+            self.assertEqual(str(type(self.ss_client)),"<class 'gdata.spreadsheet.service.SpreadsheetsService'>") 
+        except:
             self.assertRaises(SystemExit)
-            
             
     '''
     Test id= 2
-        Unit to test = glogin.LogIn.Oauth2Login()
+        Unit to test = g2sagemathlogin.LogIn.logIn()
+        Assumptions= 
+        Test data= 
+        Steps 
+               
+        Expected result=
+    '''        
+    def test_login_output1(self):
+        try:
+            self.assertEqual(str(type(self.doc_client)),"<class 'gdata.docs.service.DocsService'>")
+        except:
+            self.assertRaises(SystemExit)
+        
+    '''
+    Test id= 3
+        Unit to test = g2sagemathlogin.LogIn.logIn()
+        Assumptions= 
+        Test data= 
+        Steps 
+               
+        Expected result=
+    '''        
+    def test_login_output_size(self):
+        
+        self.assertEqual(len(self.c),2)
+               
+            
+    '''
+    Test id= 
+        Unit to test = g2sagemathlogin.LogIn.Oauth2Login()
         Assumptions= 
         Test data= 
         Steps 
@@ -48,97 +82,101 @@ class LoginTest(unittest.TestCase):
         
         Expected result= a value of type "gdata.spreadsheet.service.SpreadsheetsService"
     '''
-            
+    @unittest.skip("not implemented")      
     def test_oauth2login(self):
         pass
       
     '''
-    Test id= 3
-        Unit to test = glogin.LogIn.ShowDrive()
+    Test id= 
+        Unit to test = g2sagemathlogin.LogIn.ShowDrive()
         Assumptions= 
         Test data= 
         Steps 
                
-        Expected result= a value of type "gdata.spreadsheet.service.SpreadsheetsService"
+        Expected result=
     '''
     
     def test_showdrive_output_type(self):
-        print "---test_showdrive_output_type---"
-        l2=glogin.LogIn()
-        d=[self.client]
+        
+        d=[self.ss_client]
         try:
-            out_list=l2.showDrive(data_arg=d)
+            out_list=self.l1.showDrive(data_arg=d)
             self.assertEqual(type(out_list),type([]))
-            print "showDrive returned a list"
-        except Exception as e:
-            print "Test 3: showDrive Exception captured successfully"
+        except:
             self.assertRaises(SystemExit)
             
     
     '''
-    Test id= 4
-        Unit to test = glogin.LogIn.ShowDrive()
+    Test id= 
+        Unit to test = g2sagemathlogin.LogIn.ShowDrive()
         Assumptions= 
         Test data= 
         Steps 
                  
-        Expected result= a value of type "gdata.spreadsheet.service.SpreadsheetsService"
+        Expected result= 
     '''
              
     def test_showdrive_output_size(self):
-        print "---test_showdrive_output_size---"
-        l3=glogin.LogIn()
+        
         try:
-            out_list=l3.showDrive([self.client])
-            print len(out_list)
-            self.assertEqual(len(out_list),3)
-            print "showDrive returned a list of size 3"
-        except Exception as e:
-            print "Test 4: showDrive Exception captured successfully"
+            out_list=self.l1.showDrive([self.ss_client])
+            self.assertEqual(len(out_list),4)
+
+        except:
             self.assertRaises(SystemExit)
              
     '''
-    Test id= 5
-        Unit to test = glogin.LogIn.ShowDrive()
+    Test id= 
+        Unit to test = g2sagemathlogin.LogIn.ShowDrive()
         Assumptions= 
         Test data= 
         Steps 
                  
-        Expected result= a value of type "gdata.spreadsheet.service.SpreadsheetsService"
+        Expected result= 
     '''
      
-    def test_showdrive_output_content(self):
-        print "---test_showdrive_output_content---"
-        l4=glogin.LogIn()
+    def test_showdrive_output_content_0(self):
+        
         try:
-            out_list=l4.showDrive([self.client])
+            out_list=self.l1.showDrive([self.ss_client])
             self.assertEqual(str(type(out_list[0])),"<class 'gdata.spreadsheet.service.SpreadsheetsService'>")
+        except:
+            self.assertRaises(SystemExit)
+            
+    
+    def test_showDrive_output_content_1(self):
+        
+        try:
+            out_list=self.l1.showDrive([self.ss_client])
             self.assertEqual(str(type(out_list[1])),"<class 'gdata.GDataFeed'>")
             self.assertEqual(type(out_list[2]),type([]))
-        except Exception as e:
+        except:
+            self.assertRaises(SystemExit)
+            
+    def test_showDrive_output_content_2(self):
+        
+        try:
+            out_list=self.l1.showDrive([self.ss_client])
+            self.assertEqual(type(out_list[2]),type([]))
+        except:
             self.assertRaises(SystemExit)
              
+       
     '''
-    Test id= 6
-        Unit to test = glogin.LogIn.UserChoice()
+    Test id= 
+        Unit to test = g2sagemathlogin.LogIn.UserChoice()
         Assumptions= 
         Test data= 
         Steps 
         
          
-        Expected result= a value of type "gdata.spreadsheet.service.SpreadsheetsService"
+        Expected result= 
     '''
-     
+    @unittest.skip("not implemented")
     def test_userchoice(self):
         pass
  
      
-
-
-
-
-def main(self):
-    test1= LoginTest()
 
 
 if __name__ == "__main__":
